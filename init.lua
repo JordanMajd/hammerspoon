47290f2191ccd5d9d10a708034831e0ac10183e9
Jordan Majd <jordan.majd@galvanize.com>
2016-02-05 11:15:23 -0700

    set up caffiene to disable notifiations

--- init.lua
+++ init.lua
@@ -1,17 +1,18 @@
---Constants--
+--CONSTANTS--
 local CMD_ALT = {"cmd", "alt"}
 local CMD_ALT_CTRL = {"cmd", "alt", "ctrl"}
---/Constants--
+--/CONSTANTS--
 
---Config--
+--CONFIG--
+--no window animations--
 hs.window.animationDuration = 0
---/Config
+--/CONFIG--
 
---Layouts--
+--LAYOUTS--
 
---/Layouts--
+--/LAYOUTS--
 
---Functions--
+--FUNCTIONS--
 function toast(title, text)
 	hs.notify.new({title=title, informativeText=text}):send():release()
 end
@@ -65,55 +66,80 @@ end
 function hs.window.minimize(window)
 	window:setFrame()
 end
---/Functions--
+--/FUNCTIONS--
 
---Key Bindings--
-
+--KEY BINDINGS--
+--window right half of screen--
 hs.hotkey.bind(CMD_ALT, "right", function()
 	hs.window.focusedWindow():right()
 end)
 
+--window left half of screen--
 hs.hotkey.bind(CMD_ALT, "left", function()
 	hs.window.focusedWindow():left()
 end)
 
+--window top half of screen--
 hs.hotkey.bind(CMD_ALT, "up", function()
 	hs.window.focusedWindow():top()
 end)
 
+--window bottom half of screen--
 hs.hotkey.bind(CMD_ALT, "down", function()
 	hs.window.focusedWindow():bottom()
 end)
 
+--window full screen--
 hs.hotkey.bind(CMD_ALT, "f", function()
 	hs.window.focusedWindow():maximize()
 end)
 
+--close window--
 hs.hotkey.bind(CMD_ALT, "d", function()
 	hs.window.focusedWindow():close()
 end)
 
+--lock screen--
 hs.hotkey.bind(CMD_ALT, "l", function()
 	hs.caffeinate.lockScreen()
 end)
---/Key Bindings--
 
---Caffeine--
---Keeps screen awake when caffeinated
+--test toast--
+hs.hotkey.bind(CMD_ALT, "t", function()
+	toast("test")
+end)
+--/KEY BINDINGS--
+
+--CAFFIENE--
+--keeps screen awake when caffeinated
+--disables notifications when caffeinated
 local caffeine = hs.menubar.new()
 function toggleCaffeine()
 	if hs.caffeinate.toggle("displayIdle") then
 		caffeine:setTitle("Awake")
+		hs.execute("launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist")
 	else
 		caffeine:setTitle("Tired")
+		hs.execute("launchctl load -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist")
 	end
 end
 caffeine:setClickCallback(toggleCaffeine)
 hs.hotkey.bind(CMD_ALT, "c", toggleCaffeine)
 toggleCaffeine()
---/Caffeine--
+--/CAFFIENE--
 
---Autoreload--
+--AUTORELOAD--
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 toast("Hammerspoon","Config Reloaded.");
---/Autoreload--
+--/AUTORELOAD--
+
+--NOTES AND SUCH--
+--[[
+--sudo defaults write /System/Library/LaunchAgents/com.apple.notificationcenterui KeepAlive -bool False
+lordbunson: what sort of commands do you want to run? We have hs.execute() which is really simple and blocks while the command runs, or you can use hs.task for
+            excellent features (like running a command in the background)
+Ng: and for files, we don't provide anything specific, but there are the stock Lua library functions (specifically, the 'io' library)
+Ng:http://lua-users.org/wiki/IoLibraryTutorial is probably going to cover what you need for handling files
+< Ng:(although if you are using files to store settings/data, you might want to look at whether hs.settings can cover your needs)
+]]--
+--/NOTES AND SUCH--
