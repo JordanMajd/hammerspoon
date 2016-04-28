2cfd6038befdf862365d386080bcb5bb6c2e5dd3
Jordan Majd <jordan.majd@galvanize.com>
2016-04-27 20:41:49 -0600

    typography and styling of conky

--- lib/conky.lua
+++ lib/conky.lua
@@ -5,38 +5,48 @@ local Util = require "lib.util"
 local Conky = {
   tickRate = 10,
   background = nil,
-  backgroundDimensions = hs.geometry.rect(0, 50, 155, 335),
+  backgroundDimensions = hs.geometry.rect(0, 50, 165, 350),
   backgroundColor = {red=0, blue=0, green=0, alpha=0.65},
   text = nil,
-  textDimensions = hs.geometry.rect(10, 60, 155, 335),
+  textDimensions = hs.geometry.rect(10, 60, 165, 350),
   bodyStyle = {
     paragraphStyle = {
-      alignment = "justified"
+      alignment = "left"
     },
     font = {
-      name = hs.styledtext.defaultFonts.system,
+      name = "Monaco",
       size = 12
     },
     color = { red=0, blue=0, green=1, alpha=1 }
   },
-  headerStyle = {
+  breakStyle = {
     paragraphStyle = {
-      alignment = "justified"
+      alignment = "left"
     },
     font = {
-      name = hs.styledtext.defaultFonts.label,
+      name = ".AppleSystemUIFont",
       size = 18
     },
     color = { red=1, blue=1, green=1, alpha=1 }
   },
-  lineSpacing = 15,
+  headerStyle = {
+    paragraphStyle = {
+      alignment = "left"
+    },
+    font = {
+      name = ".AppleSystemUIFont",
+      size = 18
+    },
+    color = { red=1, blue=1, green=1, alpha=1 }
+  },
+
   netConfig = hs.network.configuration.open()
 }
 
 function Conky:init()
 
   local me = self
-
+  print(hs.styledtext.defaultFonts.label)
   self:initBackground()
   self:initText()
 
@@ -71,7 +81,7 @@ function Conky:updateText()
   local systemText = hs.host.localizedName() .. "\n"
   systemText = systemText .. self.netConfig:consoleUser() .."\n"
 
-  local systemName = hs.styledtext.new(systemText 	, self.headerStyle)
+  local systemName = hs.styledtext.new(systemText, self.headerStyle)
   text = text:__concat(systemName)
 
   local cpuUsage = self:getCpuUsage()
@@ -79,8 +89,6 @@ function Conky:updateText()
 
   local vmStats = self:getVmStats()
   text = text:__concat(vmStats)
-
-  text = text:__concat(self:getSeparator())
 
   self.text:setStyledText(text)
 
@@ -95,16 +103,16 @@ function Conky:getCpuUsage()
 
   local cpuHeader = hs.styledtext.new("CPU Usage: \n", self.headerStyle)
   returnText = returnText:__concat(cpuHeader)
-
+  returnText = returnText:__concat(self:getSeparator())
 
   local cpuStr = "Num. Cores: " .. cpuUsage.n .. "\n"
   local usage = string.format("%.2f", tonumber(cpuUsage.overall.idle) / tonumber(cpuUsage.overall.active))
-  cpuStr = cpuStr .. "- Overall  : " .. tostring(usage) .. "%\n"
+  cpuStr = cpuStr .. "- Overall: " .. tostring(usage) .. "%\n"
 
   for i, stat in ipairs(cpuUsage) do
     if stat and stat.active and stat.idle then
       local usage = string.format("%.2f", tonumber(stat.idle) / tonumber(stat.active))
-      cpuStr = cpuStr .. "- Core #" .. tostring(i) .. " : " .. tostring(usage) .. "%\n"
+      cpuStr = cpuStr .. "- Core #" .. tostring(i) .. ": " .. tostring(usage) .. "%\n"
     end
   end
 
@@ -120,10 +128,12 @@ function Conky:getVmStats()
   local returnText = hs.styledtext.new("", {})
   returnText = returnText:__concat(self:getSeparator())
 
+
   local vmStats = hs.host.vmStat()
 
   local vmHeader = hs.styledtext.new("VM Stats: \n", self.headerStyle)
   returnText = returnText:__concat(vmHeader)
+  returnText = returnText:__concat(self:getSeparator())
 
   --1024^2
   local megDiv = 1048576
@@ -145,10 +155,10 @@ function Conky:getVmStats()
 
   local megsCached = vmStats.fileBackedPages * megMulti
 
-  local vmBodyText = "In Use   : " .. string.format("%.f", megsUsed) .. "M - ".. string.format("%.f", megsUsed / totalMegs * 100) .. "%\n"
-  vmBodyText = vmBodyText .. "Free      : " .. string.format("%.f", freeMegs) .. "M - ".. string.format("%.f", freeMegs / totalMegs * 100) .. "%\n"
-  vmBodyText = vmBodyText .. "Total     : " .. string.format("%.f", totalMegs) .. "M\n"
-  vmBodyText = vmBodyText .. "Cached  : " .. string.format("%.f", megsCached) .. "M\n"
+  local vmBodyText =         "In Use: " .. string.format("%.f", megsUsed) .. "M - ".. string.format("%.f", megsUsed / totalMegs * 100) .. "%\n"
+  vmBodyText = vmBodyText .. "Free  : " .. string.format("%.f", freeMegs) .. "M - ".. string.format("%.f", freeMegs / totalMegs * 100) .. "%\n"
+  vmBodyText = vmBodyText .. "Total : " .. string.format("%.f", totalMegs) .. "M\n"
+  vmBodyText = vmBodyText .. "Cached: " .. string.format("%.f", megsCached) .. "M\n"
   local vmBody = hs.styledtext.new(vmBodyText, self.bodyStyle)
   returnText = returnText:__concat(vmBody)
 
@@ -156,7 +166,7 @@ function Conky:getVmStats()
 end
 
 function Conky:getSeparator()
-  return hs.styledtext.new("----------------\n", self.headerStyle)
+  return hs.styledtext.new("-----------------\n", self.breakStyle)
 end
 
 return Conky
